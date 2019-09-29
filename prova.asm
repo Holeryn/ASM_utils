431db61390cd8b2b6877dddca95674a5cb894955
Holeryn <53957646+Holeryn@users.noreply.github.com>
2019-09-29 15:05:53 +0000

    Add files via upload

---
+++ prova.asm
@@ -0,0 +1,21 @@
+%include "Utils.mac"
+
+segment .text
+name db "file2.text",0
+
+
+segment .bss
+buffer resb 1
+fd resw 1
+
+segment .code
+    global _start
+
+_start:
+    READ STDIN,buffer,10
+
+    WRITE STDOUT,buffer,10
+
+    CREAT name,O_CREAT
+    
+    EXIT SUCCESS_EXIT
