f0aa7fc194de62c345354789093e483673f80a85
Holeryn <53957646+Holeryn@users.noreply.github.com>
2019-09-30 19:14:24 +0000

    Update prova.asm

--- prova.asm
+++ prova.asm
@@ -7,6 +7,7 @@ name db "file2.text",0
 segment .bss
 buffer resb 1
 fd resw 1
+time resb 1
 
 segment .code
     global _start
@@ -17,5 +18,9 @@ _start:
     WRITE STDOUT,buffer,10
 
     CREAT name,O_CREAT
+
+    TIME time
+
+    WRITE STDOUT,time,100
     
     EXIT SUCCESS_EXIT
