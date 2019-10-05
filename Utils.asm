c8320164b9a853da60f8cfa3500bfda25c8317e3
Holeryn <53957646+Holeryn@users.noreply.github.com>
2019-10-05 22:44:02 +0200

    Update Utils.asm

--- Utils.asm
+++ Utils.asm
@@ -15,9 +15,23 @@
 %define O_CREAT		    00000100
 %define O_DIRECTORY     00200000
 
+;Mode of chmod
+%define S_ISUID 04000   ;Set User ID
+%define S_ISGID 02000   ;Set-group-ID
+%define S_ISVTX 01000   ;Sticky bit
+%define S_IRUSR 00400   ;Read By Owner
+%define S_IWUSR 00200   ;Write By Owner
+%define S_IXUSR 00100   ;Execute/search by owner 
+%define S_IRGRP 00040   ;Read By Group
+%define S_IXGRP 00010   ;EXECUTE/SEARCH by Group
+%define S_IROTH 00004   ;Read By Others
+%define S_IWOTH 00002   ;Write By Others
+%define S_IXOTH 00001   ;EXECUTE / SEARCH by Others 
+
 ;Valuse of PID Options
 ;meaning wait for any child process whose process group ID is
 ;equal to the absolute value of pid.
+;Returns : On success, zero is returned.  On error, -1 is returned, and errno is set appropriately.
 %define WEAITNG_ABSOULTE_PID    -2
 
 ;meaning wait for any child process
@@ -161,4 +175,80 @@
     mov     ecx,%2          ;ECX = Mode
     mov     edx,%3          ;EDX = Dev
     int     0x80            ;Call System
+%endmacro
+
+
+;Chdmod Chanfge a files mode bits
+;params :  1, pathname , mode
+;Return : On success, zero is returned.  On error, -1 is returned, and errno is set appropriately.
+%macro CHMOD 2
+    mov eax,0x0F            ;EAX = 0x0F
+    mov ebx,%1              ;EBX = Pathname
+    mov ecx,%2              ;ECX = Mode
+    int 0x80                ;Call System
+%endmacro
+
+;chown - change ownership of a file
+;Parameters : 1,pathname, 2 owner ,3 group
+;Return :On success, zero is returned.  On error, -1 is returned, and errno is set appropriately.
+%macro CHOWN 3
+    mov eax,0x10            ;EAX = 0x10
+    mov ebx,%1              ;EBX = Pathname
+    mov ecx,%2              ;ECX = Owner
+    mov edx,%3              ;EDX = Group
+    int 0x80                ;Call System
+%endmacro
+
+;Stat - Get File Status
+;Parameters : 1,pathname, 2 statbuf
+;Returns : On success, zero is returned.  On error, -1 is returned, and errno is set appropriately.
+%macro STAT 2
+    mov eax,0x12            ;EAX = 0x12
+    mov ebx,%1              ;EBX = Pathname
+    mov ecx,%2              ;ECX = StatBuf
+    int 0x80                ;Call System
+%endmacro
+
+;LSEEK - Read Write FIle Offset
+;Parameters - 1, fd(File Descriptor), 2 offset, 3 whence
+;Return : Upon successful completion, lseek() returns the resulting offset
+;location as measured in bytes from the beginning of the file.  On
+;error, the value (off_t) -1 is returned and errno is set to indicate
+;the error.
+%macro LSEEK 3
+    mov eax,0x13            ;EAX = 0x13
+    mov ebx,%1              ;EBX = File Descriptor
+    mov ecx,%2              ;ECX = Offset
+    mov edx,%3              ;EDX = whence
+    int 0x80                ;Call System
+%endmacro
+
+;GETPID - Get Process Identification
+;Paramaters - void
+;Returns : The process ID (PID) of the calling process
+%macro GETPID 0
+    mov eax,0x14            ;EAX = 0x14
+    int 0x80                ;Call System
+%endmacro
+
+;MOUNT - Mount FileSystem
+;Parameters : 1, source, 2, target,3 fylesystemtype,4, mountflags,5, data
+;Returns :  On success, zero is returned.  On error, -1 is returned, and errno is set appropriately.
+%macro MOUNT 5
+    mov eax,0x15           ;EAX = 0x15
+    mov ebx,%1             ;EBX = SOURCE
+    mov ecx,%2             ;ECX = TARGET
+    mov edx,%3             ;EDX = FYLESYSTEMTYPE
+    mov esi,%4             ;ESI = MOUNTFLAGS
+    mov edi,%5             ;EDI = DATA
+    int 0x80               ;CALL SYSTEM
+%endmacro
+
+;OLD MOUNT - Remove The attachment of the file system mounted target
+;Parameters : 1, name
+;Returns :  On success, zero is returned.  On error, -1 is returned, and errno is set appropriately.
+%macro OLD_MOUNT 1
+    mov eax,0x16           ;EAX = 0x16
+    mov ebx,%1             ;EBX = name
+    int 0x80               ;Call System
 %endmacro
